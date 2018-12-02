13f6f20c56b00bfde0f24ea95d4204fb704639b6
trouble <huhude123>
2018-12-02 14:20:23 +0800

    Second Commit

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="phonebook-container mt-5">
+        <div class="phone-header pb-3">My Phonebook
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addNewModal">Add New</button>
+        </div>
+        <div class="phone-body">
+            <input type="text" placeholder="search..." name="search" id="search" class="form-control">
+            <ul>
+                <li class="pb-4 mt-3">
+                    <div class="float-left">Sushmita</div>  
+                    <div class="float-right">
+                        <button type="button" class="btn btn-primary">Add</button>
+                        <button type="button" class="btn btn-success">Edit</button>
+                        <button type="button" class="btn btn-light">Delete</button>
+                    </div>
+                </li>
+                <div class="clearfix"></div>
+                <li class="pb-4">
+                    <div class="float-left">Lester</div>  
+                    <div class="float-right">
+                        <button type="button" class="btn btn-primary">Add</button>
+                        <button type="button" class="btn btn-success">Edit</button>
+                        <button type="button" class="btn btn-light">Delete</button>
+                    </div>
+                </li>
+                <div class="clearfix"></div>
+                <li class="pb-4">
+                    <div class="float-left">Mama</div>  
+                    <div class="float-right">
+                        <button type="button" class="btn btn-primary">Add</button>
+                        <button type="button" class="btn btn-success">Edit</button>
+                        <button type="button" class="btn btn-light">Delete</button>
+                    </div>
+                </li>
+            </ul>
+        </div>  
+    </div>
+</template>
